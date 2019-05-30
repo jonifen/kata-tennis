f7edda8ca00a5d8fb5bc9f78c7532d31434dd256
Jon Cain <jonifen@users.noreply.github.com>
2019-05-30 20:35:01 +0100

    Add C# implementation created about 2 years ago

---
+++ csharp_01/TennisKata01/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TennisKata01")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TennisKata01")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c0571eb7-0f52-47c6-9170-efc389b665fe")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ csharp_01/TennisKata01/TennisGameTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tennis;
+
+namespace TennisKata01
+{
+    [TestClass]
+    public class TennisGameTests
+    {
+        private Game _game;
+
+        [TestInitialize]
+        public void Initialise()
+        {
+            _game = new Game();
+        }
+
+        [TestMethod]
+        public void NewGame_ScoreIsLoveAll()
+        {
+            Assert.AreEqual("love all", _game.GetScore());
+        }
+
+        [TestMethod]
+        public void PlayerOneScoresFirst_ScoreIs15Love()
+        {
+            _game.PlayerOneScores();
+            Assert.AreEqual("15 love", _game.GetScore());
+        }
+
+        [TestMethod]
+        public void PlayerTwoScoresFirst_ScoreIsLove15()
+        {
+            _game.PlayerTwoScores();
+            Assert.AreEqual("love 15", _game.GetScore());
+        }
+
+        [TestMethod]
+        public void BothPlayersScore_ScoreIs15All()
+        {
+            _game.PlayerOneScores();
+            _game.PlayerTwoScores();
+            Assert.AreEqual("15 all", _game.GetScore());
+        }
+    }
+}
