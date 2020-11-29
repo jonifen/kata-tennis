8cdbf017398a62444facbff3695fe71b8d542fe0
Jon Cain <jonifen@users.noreply.github.com>
2020-11-29 13:45:32 +0000

    Repo restructure

---
+++ Tennis/Game.cs
@@ -0,0 +1,29 @@
+﻿namespace Tennis
+{
+    public class Game
+    {
+        private ScoreBoard _scoreBoard;
+
+        public Game()
+        {
+            _scoreBoard = new ScoreBoard(0, 0);
+        }
+
+        public void PlayerOneScores()
+        {
+            _scoreBoard = new ScoreBoard(_scoreBoard.PlayerOneScore + 1, _scoreBoard.PlayerTwoScore);
+        }
+
+        public void PlayerTwoScores()
+        {
+            _scoreBoard = new ScoreBoard(_scoreBoard.PlayerOneScore, _scoreBoard.PlayerTwoScore + 1);
+        }
+
+        public string GetScore()
+        {
+            return _scoreBoard.ToString();
+        }
+
+
+    }
+}
